05e421d37d73801ffb46b8dff54d01779f07436a
Heber Rocha <heberonly@gmail.com>
2022-07-11 10:41:31 -0400

    Update plot_comparison.py

--- Verhulst/KernelComparison/plot_comparison.py
+++ Verhulst/KernelComparison/plot_comparison.py
@@ -11,21 +11,20 @@ from matplotlib import pyplot as plt
 import matplotlib
 
 # Latex font rendering
-matplotlib.rc('font',**{'family':'serif','serif':['Computer Modern Roman']})
-matplotlib.rc('text', usetex = True)
+# matplotlib.rc('font',**{'family':'serif','serif':['Computer Modern Roman']})
+# matplotlib.rc('text', usetex = True)
 
 
 # Loading data
 data = np.loadtxt("modeldata.dat", delimiter = '\t')
-qoi = data[:, 1]
-qoi_RBF, qoi_RBF_std = np.loadtxt("sqrexp_qoi.txt"), np.loadtxt("sqrexp_qoi_std.txt") 
-qoi_Matern_15, qoi_Matern_15_std = np.loadtxt("Matern_qoi.txt"), np.loadtxt("Matern_qoi_std.txt") 
-qoi_Dot, qoi_Dot_std = np.loadtxt("Dot_qoi.txt"), np.loadtxt("Dot_qoi_std.txt") 
-qoi_Per, qoi_Per_std = np.loadtxt("Expsinsquared_qoi.txt"), np.loadtxt("Expsinsquared_qoi_std.txt")
 t = data[:, 0]
+qoi = data[:, 1]
+qoi_RBF, qoi_RBF_std = np.loadtxt("sqrexp_qoi.txt"), np.loadtxt("sqrexp_qoi_std.txt")
+qoi_Matern_15, qoi_Matern_15_std = np.loadtxt("Matern_qoi.txt"), np.loadtxt("Matern_qoi_std.txt")
+qoi_Dot, qoi_Dot_std = np.loadtxt("Dot_qoi.txt"), np.loadtxt("Dot_qoi_std.txt")
+qoi_Per, qoi_Per_std = np.loadtxt("Expsinsquared_qoi.txt"), np.loadtxt("Expsinsquared_qoi_std.txt")
 
-
-plt.figure(1)
+plt.figure(figsize=(8,6.3))
 
 # Old plot
 # plt.errorbar(t, qoi, 200, marker='o', color='k', label='Data', linestyle = "None", alpha=0.8)
@@ -33,8 +32,13 @@ plt.figure(1)
 #plt.plot(t, qoi_Matern_15, marker='v', color='#0b57b0', label='Matérn ($\\nu = 1.5)$', linestyle = "dashed", markersize = 10, alpha=0.62)
 #plt.plot(t, qoi_Dot, marker='h', color='#0b8ab0', label='Dot', markersize = 9, linestyle = "dotted", alpha=0.62)
 #plt.plot(t, qoi_Per, marker='d', color='#1529bf', label='Periodic', markersize = 9, alpha=0.92)
+# qoi_RBF_std = np.array(len(t)*[qoi_RBF_std])
+# qoi_Matern_15_std = np.array(len(t)*[qoi_Matern_15_std])
+# qoi_Dot_std = np.array(len(t)*[qoi_Dot_std])
+# qoi_Per_std = np.array(len(t)*[qoi_Per_std])
 
 # New plot
+plt.scatter(0,1000.0,200,color='w')
 plt.errorbar(t, qoi, 200, marker='o', color='k', label='Data', linestyle = "None", alpha=0.8)
 plt.plot(t, qoi_RBF, marker='^', color='#8B4513', label='Sqr. exp', markersize = 10, alpha=0.95)
 plt.fill_between(t,  qoi_RBF - 1.96 * qoi_RBF_std, qoi_RBF + 1.96 * qoi_RBF_std, color='#8B4513', alpha=0.12)
@@ -45,13 +49,14 @@ plt.plot(t, qoi_Dot, marker='h', color='red', label='Dot', markersize = 9, lines
 plt.plot(t, qoi_Per, marker='d', color='#1E90FF', label='Periodic', markersize = 9, alpha=0.92)
 # plt.fill_between(t,  qoi_Per - 1.96 * qoi_Per_std, qoi_Per + 1.96 * qoi_Per_std, color='#1E90FF', alpha=0.12)
 
-
-plt.xlabel('Time (hours)', size = 17)
-plt.ylabel('Number of Cells', size = 17)
-plt.xlim([0,290])
+plt.xticks(fontsize=16)
+plt.yticks(fontsize=16)
+plt.xlabel('Time (hours)', size = 16)
+plt.ylabel('Number of Cells', size = 16)
+# plt.xlim([0,290])
 #plt.ylim([-800, 11000])
-plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.1),
-          fancybox=True, shadow=True, ncol=5, fontsize = 12)
+plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.22),
+          fancybox=True, shadow=True, ncol=3, fontsize = 16)
 
-plt.show()
-
+plt.savefig("kernel_comp.jpg", dpi=120,bbox_inches='tight')
+# plt.show()
